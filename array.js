437aef92e9c46d5ded95818f8d886b9768c93ee9
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2022-02-22 15:02:08 -0500

    Update array.js

--- array.js
+++ array.js
@@ -1,14 +1,18 @@
 nombres=["francisco","Sergio","juana","mariana","jose","juan daniel","daniela","Mateo","Samuel","Ana","jorge","Nicolas","Gabriela","Daniel","Jacobo","Tomas","Sofia"]
 edades=[]
+let suma= 0
 for(let i=0;i<17;i++){
     do
     {
         edad=Number(prompt("ingrese la edad de  " + nombres[i]))
     }
     while( isNaN(edad) || edad<10 || edad>17)
+    edades.push(edad)
 
 }
-suma= suma+edad[i]
+for(let i=0; i<17; i++){
+suma=suma+edades[i]
+}
 promedio=suma/17
 let contenido=document.querySelector("#informacion")
 informacion.innerHTML= "el Promedio de las notas es "+ promedio
